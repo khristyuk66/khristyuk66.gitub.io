d9d3f01ada92d9c92e49ca3d8df0962268b7d6c3
khristyuk66 <39514052+khristyuk66@users.noreply.github.com>
2018-10-18 12:04:20 +0300

    Add files via upload

---
+++ lb2.js
@@ -0,0 +1,29 @@
+﻿function $()
+{ var elements = new Array();
+for (var i = 0; i < arguments.length; i++)
+{ var element = arguments[i];
+if (typeof element == "string")
+element = document.getElementById(element);
+if (arguments.length == 1)
+return element;
+elements.push(element);
+}
+return elements; }
+
+function tg1()
+{
+alert('Пауза');
+if($("sl1").selectedIndex==0)
+$("MyDiv").style.color="red";
+
+if($("sl1").selectedIndex==1)
+$("MyDiv").style.color="pink";
+
+
+if($("sl1").selectedIndex==2)
+$("MyDiv").style.color="green";
+
+
+if($("sl1").selectedIndex==3)
+$("MyDiv").style.color="blue";
+}
