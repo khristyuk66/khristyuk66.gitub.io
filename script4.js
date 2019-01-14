cd62e2e4b9529dc324e16c1d7553ccdeec58a006
khristyuk66 <39514052+khristyuk66@users.noreply.github.com>
2019-01-14 10:50:43 +0200

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,31 @@
+﻿﻿﻿﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
+
+function middle() {
+
+var N = parseInt(document.getElementById('N').value);
+var K = parseInt(document.getElementById('K').value);
+
+var summ=0;
+for (var i=1;i<=N;i++){
+
+summ= summ + Math.pow(i,K);
+
+
+//console.log(summ);
+document.getElementById('result').innerHTML = summ;
+
+
+
+}
+
+}
---
+++ script5.js
@@ -0,0 +1,24 @@
+﻿﻿﻿﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
+function middle()
+
+{
+var A = parseInt(document.getElementById('A').value),
+B = parseInt(document.getElementById('B').value),
+ i, j, Nums = [];		
+for (i = A; i <= B; i++)		
+{
+for (j = 0; j < i; j++) Nums.push(i);
+}
+console.log(Nums);
+document.getElementById('result').innerHTML = Nums.join(", ");
+}
