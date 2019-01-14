25592a3dc5bbee383b79bd1739486c0528af9bbb
khristyuk66 <39514052+khristyuk66@users.noreply.github.com>
2019-01-14 22:19:17 +0200

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,64 @@
+﻿﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
+var today = new Date();
+var day
+var month
+var year
+var d
+function f(x)
+	{
+		$("tx1").value = $("tx1").value+x;
+	}
+function f1()
+  {
+     if($("tx1").value <= 31)
+    {
+      day = $("tx1").value;
+    }
+    else
+    {
+    alert('Значение не должно быть выше 31');
+    }
+    $("tx1").value = '';
+  } 
+function f2()
+  { 
+    if($("tx1").value <= 12)
+    {
+      month = $("tx1").value;
+    }
+    else
+    {
+    alert('Значение не должно быть выше 12');
+    }
+      $("tx1").value = '';    
+  }
+function f3()
+  { 
+    year = $("tx1").value;
+    $("tx1").value = '';
+  }
+function f4()
+  {
+    d = new Date(year,month,day);
+    $("tx2").value = getWeekDay(d);
+    $("tx3").value = datediff(d,today);
+  }
+function getWeekDay(date) {
+  var days = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
+
+  return days[date.getDay()];
+}
+function datediff(first,second)
+  {
+    return Math.round((second-first)/(1000*60*60*24));
+  }
---
+++ gif.js
@@ -0,0 +1,32 @@
+﻿﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
+var t;
+var r1=255;
+var g1=255;
+var b1=255;
+function f()
+{
+var x1=document.getElementById('b11');
+x1.style.left=(parseInt(x1.style.left)+100)+'px';
+x1.style.top=(parseInt(x1.style.top)+100)+'px';
+r1-=100;
+g1-=50;
+b1-=100;
+str1='rgb('+r1+', '+g1+', '+b1+')';
+x1.style.backgroundColor=str1;
+}
+function h()
+{t=setInterval('f()',600);}
+function g()
+{
+clearInterval(t);
+}
